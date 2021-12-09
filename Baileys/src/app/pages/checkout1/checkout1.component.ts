4f4c94f7f719454399de8935fcad19a3fd6d8956
Akeem McKenzie <Akeem.McKenzie@suddath.com>
2021-12-09 02:46:09 -0500

    hb

--- Baileys/src/app/pages/checkout1/checkout1.component.ts
+++ Baileys/src/app/pages/checkout1/checkout1.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-checkout1',
   templateUrl: './checkout1.component.html',
-  styleUrls: ['./checkout1.component.css']
+  styleUrls: ['./checkout1.component.css'],
 })
 export class Checkout1Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
